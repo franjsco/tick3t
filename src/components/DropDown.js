164c055e7c5a209fc19365579dbce30e9ba3b275
frab1t <33671357+frab1t@users.noreply.github.com>
2019-05-08 16:17:26 +0200

    add dropdown component

---
+++ src/components/DropDown.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Input } from 'reactstrap';
+
+
+class DropDown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    let options = this.props.options;
+    options = options.map((opt) => {
+      return <option key={opt.id} value={opt.id}>{opt.value}</option>
+    });
+    console.log('reh');
+
+  return(
+    <Input
+      name = "type"
+      type = "select"
+      value = { this.props.type }
+      onChange = { this.props.handleInputChange }
+    >
+      <option value="">---</option>
+      { options }
+    </Input>
+    );
+  }
+}
+
+export default DropDown;

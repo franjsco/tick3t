38b07ea8628ffab4f52a702fa6733328bc267854
frab1t <33671357+frab1t@users.noreply.github.com>
2019-05-10 16:23:07 +0200

    refactoring and added new routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './containers/NavBar';
-import Home from './containers/Home/Home';
-import OpenTicket from './containers/OpenTicket/OpenTicket';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import PageNotFound from './containers/PageNotFound';
+
+import NavBar from './components/NavBar';
+import Home from './containers/HomePage/Home';
+import CreateTicket from './containers/CreateTicket/CreateTicket';
 import ViewRequest from './containers/ViewTicket/ViewTicket';
-import { Container } from 'reactstrap';
-import Login from './containers/Admin/Login';
+import PageNotFound from './containers/PageNotFound';
+import Login from './containers/Administration/Login';
+import TicketList from './containers/Administration/TicketList';
+import TicketWork from './containers/Administration/TicketManager';
 
 function App() {
   return (
@@ -18,10 +22,13 @@ function App() {
         <Container>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/open" component={OpenTicket} />
+          <Route path="/create" component={CreateTicket} />
           <Route path="/ticket/:ticketId" component={ViewRequest} />
           <Route path="/ticket/" exact component={ () => <Redirect to="/" /> } />
           <Route path="/login" component={Login} />
+          <Route path="/admin/" exact component={ () => <Redirect to="/admin/ticket" /> } />
+          <Route path="/admin/ticket" exact component={TicketList} />
+          <Route path="/admin/ticket/:ticketId" component={TicketWork} />
           <Route component={PageNotFound} />
         </Switch>
         </Container>

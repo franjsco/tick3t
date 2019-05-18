8278b4eb3612bc5f6b57a3b59068c356be8ae1e3
frab1t <33671357+frab1t@users.noreply.github.com>
2019-05-18 19:09:11 +0200

    feat: add custom button component

---
+++ src/components/Button.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+  Button as ButtonBootstrap
+} from 'reactstrap';
+
+const style = {
+  borderRadius: 20
+};
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <ButtonBootstrap
+        tag={this.props.tag}
+        to={this.props.to}
+        color="primary"
+        outline
+        style={style}
+      >
+        {this.props.children}   
+      </ButtonBootstrap>
+      
+    );
+  }
+}
+
+export default Button;

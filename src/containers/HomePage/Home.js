c5d3138344d0d1b3024e77717e3dfed7acb45d17
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 16:09:21 +0200

    refactor: Home component

--- src/containers/HomePage/Home.js
+++ src/containers/HomePage/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Row,
   Col
@@ -7,25 +7,17 @@ import {
 import CreateTicketCard from './CreateTicketCard';
 import SearchTicketCard from './SearchTicketCard';
 
-
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Row>
-        <Col sm={12} md={6}>
-          <CreateTicketCard />
-        </Col>
-        <Col sm={12} md={6}>
-          <SearchTicketCard />
-        </Col>
-      </Row>
-    );
-  }
+const Home = (props) => {
+  return (
+    <Row> 
+      <Col sm={12} md={6}>
+        <CreateTicketCard />
+      </Col>
+      <Col sm={12} md={6}>
+        <SearchTicketCard />
+      </Col>
+    </Row>
+  );
 }
 
 export default Home;

2a46594dde62085b6d047fa5fa52c719a4dfc4e3
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 16:02:16 +0200

    improvement: edit App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import { Container } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-
 import Routes from './Routes';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <NavBar />
-
         <Container>
           <Footer />
         </Container>
-
       </Routes>
     </div>
   );

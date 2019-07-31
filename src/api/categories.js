e7793fe063f5679cf8493d4c7f38f86e81c05f8a
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 15:57:44 +0200

    feat(api): add new methods categories

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,16 +1,47 @@
 import { config } from '../config';
-import { httpClient } from './httpClient';
 
 export const getAllTicketStatus = () => {
-
   const header = new Headers({
     'Content-Type': 'application/json'
   });
 
-    const options = {
-      method: 'GET',
-      headers: header
-    };
+  const options = {
+    method: 'GET',
+    headers: header
+  };
 
-    return httpClient(`${config.baseURL}categories?type=TicketStatus`, options)
+  return fetch(`${config.baseURL}categories?type=TicketStatus`, options)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else if (res.status === 404) {
+        throw new Error('Categories not found.');
+      } else {
+        throw new Error('Ops,problem');
+      };
+    });
+}
+
+
+export const getAllTicketType = () => {
+  const header = new Headers({
+    'Content-Type': 'application/json'
+  });
+
+  const options = {
+    method: 'GET',
+    headers: header
+  };
+
+
+  return fetch(`${config.baseURL}categories?type=TicketType`, options)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else if (res.status === 404) {
+        throw new Error('Categories not found.');
+      } else {
+        throw new Error('Ops,problem');
+      };
+    });
 }

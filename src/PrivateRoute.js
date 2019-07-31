530f22c77e69c12c66268ce233b29f55da650485
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 16:01:12 +0200

    improvement: edit PrivateRoutes

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-import { isLogin } from "./utils/auth";
-
 export const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={props =>
-        isLogin() ? <Component {...props} /> : <Redirect to="/login" />
-      }
+      render={props => (
+        localStorage.getItem('user')
+          ? <Component {...props} /> 
+          : <Redirect to={{ pathname: '/login', state: { from: props.location }}} />
+      )}
     />
   );
 };

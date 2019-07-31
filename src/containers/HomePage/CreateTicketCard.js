aa04db30c0630270271b70d664fcf866c4bd3907
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 16:08:51 +0200

    refactor: CreateTicketCard

--- src/containers/HomePage/CreateTicketCard.js
+++ src/containers/HomePage/CreateTicketCard.js
@@ -1,46 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Card from '../../components/Card';
 import Logo from '../../components/Logo';
 import Button from '../../components/Button';
 
-import logoSVG from '../../assets/assistance.svg';
+import logoCreateTicket from '../../assets/create_ticket.svg';
 
 const style = {
   height: '160px',
   margin: '30px'
 };
 
-class CreateTicketCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Card
-        headAlign="center"
-        title="Create Ticket"
-        subtitle="Create a new ticket"
-        bodyAlign="center"
-      >
-        <Logo
-          style={style}
-          src={logoSVG}
-          alt="Create Ticket" 
-        />
-
-        <Button
-          tag={Link}
-          to="/create"
-        >
-          Create Ticket
-        </Button>
-      </Card>
-    );
-  }
+const CreateTicketCard = (props) => {
+  return (
+    <Card
+      headAlign="center"
+      title="Create Ticket"
+      subtitle="Create a new ticket"
+      bodyAlign="center" >
+      <Logo
+        style={style}
+        src={logoCreateTicket}
+        alt="Create Ticket" />
+      <Button
+        tag={Link}
+        to="/create" >
+        Create Ticket
+      </Button>
+    </Card>
+  );
 }
+
 
 export default CreateTicketCard;

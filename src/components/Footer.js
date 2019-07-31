6df8d5e08665aa754ae41a0ec3e755226ccd84c3
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 15:54:37 +0200

    refactor: Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Row,
   Col
 } from 'reactstrap';
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Row className="row-centred">
-        <Col>
-          <p className="text-center">
-            <a href="https://github.com/frab1t/tick3t">tick3t</a>{` `}
-            by <a href="https://github.com/frsposito">@frsposito</a> (Francesco Esposito)
-          </p>
-        </Col>
-      </Row>
-    );
-  }
+const Footer = (props) => {
+  return (
+    <Row className="row-centred">
+      <Col>
+        <p className="text-center">
+          <a href="https://github.com/frsposito/tick3t">tick3t</a>{` `}
+          by <a href="https://github.com/frsposito">@frsposito</a> (Francesco Esposito)
+        </p>
+      </Col>
+    </Row>
+  );
 }
 
 export default Footer;

d6502499726700f49ff70087264e710a9212a233
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-07-31 16:08:23 +0200

    refactor: PageNotFound component

--- src/containers/PageNotFound.js
+++ src/containers/PageNotFound.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '../components/Card';
 
-class PageNotFound extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Card 
-          title="404 - Page not found"
-          footerLink={{path:"/", name:"Back to home"}}
-        >
-          <p>The page is not found</p>
-        </Card>
-      </div>
-    );
-  }
+const PageNotFound = (props) => {
+  return (
+    <div>
+      <Card 
+        title="404 - Page not found"
+        footerLink={{path:"/", name:"Back to home"}}
+      >
+        <p>The page is not found</p>
+      </Card>
+    </div>
+  );
 }
 
 export default PageNotFound;

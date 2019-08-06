5a198987beddc1234a9b47cee279568ed638ba92
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-08-06 16:46:46 +0200

    feat: add change password api

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,11 @@
 import { config } from '../config';
+import { authHeader } from '../_helpers';
 
 
 export const userAPI = {
   login,
   logout,
+  changePassword,
 };
 
 function login(email, password) {
@@ -30,18 +32,43 @@ function logout() {
 function handleResponse(response) {
   return response.text()
     .then(text => {
-        const data = text && JSON.parse(text);
+      const data = text && JSON.parse(text);
 
-        if (!response.ok) {
-          if (response.status === 401) {
-            logout();
-          }
-
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
+      if (!response.ok) {
+        if (response.status === 401) {
+          logout();
         }
 
-        return data;
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
       }
+
+      return data;
+    }
     );
 };
+
+
+function changePassword(user) {
+  const header = new Headers({
+    'Content-Type': 'application/json',
+    'authorization': authHeader()
+  });
+
+  const options = {
+    method: 'POST',
+    headers: header,
+    body: JSON.stringify({
+      newPassword: user.newPassword,
+    })
+  };
+
+  return fetch(`${config.baseURL}users/changePassword`, options)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('Ops,problem');
+      }
+    });
+}

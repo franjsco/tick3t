5fe12f8db6141c62bc23faa1476a5294b04df6d2
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-08-06 17:39:27 +0200

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,27 @@ import {
   Col
 } from 'reactstrap';
 
+const mainCredits = {
+  fontSize: '14px',
+  color: 'grey'
+}
+
+const othersCredits = {
+  fontSize: '10px',
+  color: 'grey'
+}
 const Footer = (props) => {
   return (
     <Row className="row-centred">
       <Col>
-        <p className="text-center">
+        <p style={mainCredits} className="text-center">
           <a href="https://github.com/frsposito/tick3t">tick3t</a>{` `}
           by <a href="https://github.com/frsposito">@frsposito</a> (Francesco Esposito)
+        </p>
+        
+        <p style={othersCredits} className="text-center">
+        Icon made by <a href="https://www.flaticon.com/authors/eucalyp">eucalyp</a> {` `}
+         from <a href="https://www.flaticon.com/">flaticon.com</a>
         </p>
       </Col>
     </Row>
